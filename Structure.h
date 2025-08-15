aa2bdd4c7293b42ee6ff2aee1e29a298a9e5761a
Paly Linx <linxpaly@gmail.com>
2025-08-15 19:38:58 +0200

    Create Structure.h
    La base de la structure en comptabilité .

---
+++ Structure.h
@@ -0,0 +1,6 @@
+typedef struct {
+    int ID;                 // Identifiant unique de l'opération
+    char Libelle[100];      // Libellé descriptif (100 caractères max)
+    double Credit;          // Montant crédité
+    double Debit;           // Montant débité
+} Operation;
