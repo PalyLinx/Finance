69abe8d95fe34e7d11c0e3cd76ca68c7f430adfc
Paly Linx <linxpaly@gmail.com>
2025-08-15 23:45:27 +0200

    Update Structure.h
    Mise a jour structure operation

--- Structure.h
+++ Structure.h
@@ -1,4 +1,5 @@
-typedef struct {
+
+typedef struct Operation{
     int ID;                 // Identifiant unique de l'opération
     char Libelle[100];      // Libellé descriptif (100 caractères max)
     double Credit;          // Montant crédité
